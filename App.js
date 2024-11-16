51d15e6cbb494d980d299b7a0fcfbe5b33513693
João Pedro Sabino Garcia <149002145+unifjgarcia@users.noreply.github.com>
2024-11-16 03:19:23 -0300

    Create App.js
    Função principal do codigo.

---
+++ App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+// Importar telas
+import TelaInicio from './TelaInicio';
+import RegistrarPartida from './RegistrarPartida';
+import Historico from './Historico';
+import Estatisticas from './Estatisticas';
+
+const Stack = createStackNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="TelaInicio">
+        <Stack.Screen name="TelaInicio" component={TelaInicio} />
+        <Stack.Screen name="RegistrarPartida" component={RegistrarPartida} />
+        <Stack.Screen name="Historico" component={Historico} />
+        <Stack.Screen name="Estatisticas" component={Estatisticas} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
